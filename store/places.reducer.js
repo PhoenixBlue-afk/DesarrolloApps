51023ddfab80a6f52fa9b0f946a8558a4c6c628a
PhoenixBlue-afk <82279633+PhoenixBlue-afk@users.noreply.github.com>
2023-03-09 18:16:56 -0300

    Add files via upload

---
+++ store/places.reducer.js
@@ -0,0 +1,21 @@
+import {ADD_PLACE} from "./places.actions"
+import Place from "../models/Place.js"
+
+const initialState = {
+    places: []
+}
+
+export default (state = initialState, action) => {
+    switch (action.type) {
+        case ADD_PLACE:
+            
+        const newPlace = new Place(Date.now(), action.payload.title)
+        return{
+            ...state,
+            places: state.places.concat(newPlace),
+        }
+    
+        default:
+        return state
+    }
+}
