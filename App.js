eb022df5c198862ae78660e4951adfda9ccf80e3
PhoenixBlue-afk <82279633+PhoenixBlue-afk@users.noreply.github.com>
2023-03-14 23:15:20 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+// navigation
+import MainNavigator from './navigation'
+import { Provider } from 'react-redux'
+import React from 'react';
+import { init } from './db';
+import store from './store'
+
+init()
+.then(()=>console.log('Database initialized'))
+.catch(err => {console.log('Database failed'),console.log(err.message)})
+
+
+
+export default function App() {
+  return (<Provider store={store}><MainNavigator /></Provider>);
+}
---
+++ screens/PlaceDetailScreen.js
@@ -0,0 +1,71 @@
+import { Image, ScrollView, StyleSheet, Text, View } from 'react-native'
+
+import { COLORS } from '../constants'
+import MapPreview from '../components/MapPreview'
+import React from 'react'
+import { useSelector } from 'react-redux'
+
+const PlaceDetailScreen = ({route}) => {
+
+    const {placeId}=route.params
+    const place= useSelector(state=>state.places.places.find(item=>item.id === placeId))
+
+    return (
+        <ScrollView contentContainerStyle={styles.container}>
+            <Image source={{uri:place.image}} style={styles.image}/>
+            <View style={styles.location}>
+                <View style={styles.addressContainer}>
+                    <Text style={styles.address}>{place.address}</Text>
+                </View>
+                <MapPreview 
+                style={styles.map}
+                location={{lat:place.lat, lng:place.lng}}>
+                    <Text>Ubicacion no disponible</Text>
+                </MapPreview>
+            </View>
+            </ScrollView>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      alignItems: "center",
+    },
+    image: {
+      height: "35%",
+      minHeight: 300,
+      width: "100%",
+      padding: 20,
+    },
+    location: {
+      margin: 20,
+      width: "90%",
+      maxWidth: 350,
+      backgroundColor: "white",
+      shadowColor: "black",
+      shadowOffset: { width: 0, height: 2 },
+      shadowOpacity: 0.25,
+      shadowRadius: 8,
+      elevation: 5,
+      borderRadius: 10,
+    },
+    addressContainer: {
+      padding: 20,
+    },
+    address: {
+      color: COLORS.MAROON,
+      textAlign: "center",
+    },
+    map: {
+      height: 300,
+    },
+    textContainer: {
+      padding: 20,
+    },
+    title: {
+      fontSize: 15,
+    },
+  });
+
+export default PlaceDetailScreen
